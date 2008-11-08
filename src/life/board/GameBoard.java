652a32392afa3a77ad3164543612259341fb3c91
Ian Esling <ian@BayStreet.(none)>
2008-11-08 13:56:26 +0000

    cell configuration encapsulated in CellFactory

--- src/life/board/GameBoard.java
+++ src/life/board/GameBoard.java
@@ -8,8 +8,9 @@ import java.util.*;
  */
 public class GameBoard
 {
+    CellFactory cellFactory;
     List<Cell> board;
-    public int totalRows, totalColumns;
+    int totalRows, totalColumns;
     public List<RuleHandler> rules = new ArrayList<RuleHandler>();
     List<TickListener> tickListeners = new ArrayList<TickListener>();
     public final static int defaultRows = 300, defaultColumns = 300;
@@ -21,7 +22,10 @@ public class GameBoard
 
     public GameBoard(int rows, int columns)
     {
-        newBoard(rows, columns);
+        totalColumns = columns;
+        totalRows = rows;
+        cellFactory = new CellFactory(this);
+        board = cellFactory.createGameBoardCells();
         setRules();
     }
 
@@ -42,56 +46,6 @@ public class GameBoard
     {
         return board;
     }
-
-    protected void newBoard(int rows, int columns)
-    {
-        this.totalRows = rows;
-        this.totalColumns = columns;
-        createBoard(rows, columns);
-        makeCellsNeighbourAware();
-    }
-
-    private void makeCellsNeighbourAware()
-    {
-        Cell[][] cells = new Cell[totalRows][totalColumns];
-        for (Cell cell : board)
-        {
-            cells[cell.getRow() - 1][cell.getColumn() - 1] = cell;
-        }
-
-        for (Cell cell : board)
-        {
-            for (int row = startIndex(cell.getRow()); row <= endRow(cell.getRow()); row++)
-            {
-                for (int column = startIndex(cell.getColumn()); column <= endColumn(cell.getColumn()); column++)
-                {
-                    if (!(cell.getRow() == row && cell.getColumn() == column))
-                    {
-                        addCellListener(cells, cell, row, column);
-                    }
-                }
-            }
-        }
-    }
-
-    protected void addCellListener(Cell[][] cells, Cell cell, int row, int column)
-    {
-        cell.addCellListener(cells[row - 1][column - 1]);
-    }
-
-    private void createBoard(int rows, int columns)
-    {
-        board = new ArrayList<Cell>();
-        for (int row = 1; row <= rows; row++)
-        {
-            for (int column = 1; column <= columns; column++)
-            {
-                addCellToBoard(row, column);
-            }
-        }
-    }
-
-    protected void addCellToBoard(int row, int column) {board.add(new Cell(row, column));}
 
     public void tick()
     {
@@ -128,15 +82,6 @@ public class GameBoard
         }
         return board.get(board.indexOf(cell));
     }
-
-    private int endColumn(int cellColumn)
-    {
-        return (totalColumns == cellColumn ? totalColumns : cellColumn + 1);
-    }
-
-    private int endRow(int cellRow) {return (totalRows == cellRow ? totalRows : cellRow + 1);}
-
-    private int startIndex(int idx) {return (idx == 1 ? 1 : idx - 1);}
 
     public void setDieIfLessThanTwoLiveNeighboursRule()
     {
